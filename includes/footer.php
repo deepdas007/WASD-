4e6c87265d0d27511016abc552964c8a59743e8d
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-06-16 22:54:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,113 @@
+<style type="text/css">
+
+	.facebook,.twitter,.instagram,.github,.gplus
+	{
+		transition: 400ms ease;
+	}
+
+	.facebook:hover,.twitter:hover,.instagram:hover,.github:hover,.gplus:hover
+	{
+		transform: scale(1.2);
+	}
+
+	#footer.container
+	{
+		background: #000;
+	}
+
+	.button>input[type="submit"]:hover
+	{
+		background: transparent;
+	}
+</style>
+<div class="video">
+	<div id="footer"><!--  //footer start -->
+		<div class="container"> <!-- //container start -->
+			<div class="row"> <!-- //row start -->
+				
+				<div class="col-md-3 col-sm-6"> <!-- // colmd1 open -->
+					<h4 style="color: white;">Pages</h4>
+					<ul>
+						<li><a href="cart.php">Shopping Cart</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
+						<li><a href="shop.php">Store</a></li>
+						<li><a href="checkout.php.php">My Account</a></li>
+					</ul>
+					
+					<hr class="hidden-md hidden-lg hidden-sm">
+					<h4 style="color: white;">User Section</h4>
+					<ul>
+						<li><a href="checkout.php">Login</a></li>
+						<li><a href="customer_registration.php">Register</a></li>
+					</ul>
+					<hr class="hidden-md hidden-lg hidden-sm"><!--  //hidden for samll medium and small device bootstrap -->
+				</div><!--  // colmd 1 closed -->
+
+				<div class="col-md-3 col-sm-6"><!--  //colmd 2 open -->
+					<h4 style="color: white;">Top Game Categories</h4>
+					<ul>
+						<li><a href = "https://www.microsoft.com/en-us/store/b/pc" target="_blank"> PC </a></li>
+						<li><a href = "https://www.xbox.com/en-IN/" target="_blank"> XBox </a></li>
+						<li><a href = "https://www.sony.co.in/electronics/playstation" target="_blank"> PS4 </a></li>
+					</ul>	
+					<hr class="hidden-md hidden-lg">			
+				</div><!--  //colmd 2 closed -->
+
+				<div class="col-md-3 col-sm-6"><!--  //colmd 3 open -->
+					<h4 style="color: white;">Where To Find Us</h4>
+					<p style="color: #868889;">
+						<strong>github.com/Project18410</strong>
+						<br>4 BCA "A"
+						<br>Christ University
+						<br>Bangalore
+						<br>+91 6362136158
+					</p>
+					<a href="contact.php">Go to Contact US Page</a>	
+					<hr class= "hidden-md hidden-lg">		
+				</div><!--  //colmd 3 closed -->
+
+				<div class="col-md-3 col-sm-6"><!--  //colmd 4 start -->
+					<h4 style="color: white;"> Get latest News & Update </h4>
+					<p class="text-muted" style="color: #868889;">
+						SUSCRIBE US ON SOCIAL MEDIA
+					</p>
+					<form action="" method="post" class="button">
+						<div class="input=group">
+							<input type="text" name="email" class="form-control" style="background: transparent;color: white;">
+							<br>
+							<span class="input-group-btn">
+								<input type="submit" class="btn btn-default" value="Suscribe" style="border-radius: 5px;">
+							</span>
+						</div>
+					</form>
+					<hr>
+					<h4 style="color: white;">Stay In Touch</h4>
+					<p class="social">
+						<a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+						<a href="#" class="github"><i class="fa fa-github"></i></a>
+						<a href="#" class="gplus"><i class="fa fa-google-plus"></i></a>
+					</p>
+				</div><!--  //colmd 4 end -->
+			</div> <!-- //row closed -->
+		</div> <!-- //container closed -->
+	</div> <!-- //footer closed -->
+
+	<!-- //copyright Section -->
+	<div id="copyright">
+		<div class="container">
+			<div class="col-md-6">
+				<p class="pull-left">
+					&copy; 2020 Mr. Mishan Regmi
+				</p>
+			</div>
+			<div class="col-md-6">
+				<p class="pull-right">
+					A Website By: <a href="www.github.com/RGMishan"> WASD GAMING </a>	
+				</p>			
+			</div>
+		</div>
+		
+	</div>
+</div>
