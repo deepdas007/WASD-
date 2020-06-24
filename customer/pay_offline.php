0638eaca5ff768e9e875dfc3d6fde89cf6732d51
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-06-24 06:38:32 +0530

    Add files via upload

---
+++ customer/pay_offline.php
@@ -0,0 +1,27 @@
+<div class="box">
+	<center>
+		<h1>Pay Offline Using Below Method</h1>
+		<p>If there is any problem in payment feel free to<a href="../contactus.php"> contact us</a> .</p>
+	</center>	
+	<hr>
+	<div class="table_responsive">
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<tr>
+					<th>Bank Account Details</th>
+					<th>Pay Pal Id</th>
+					<th>PayuMoney Link</th>
+					<th>PayTm/Gpay Number</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>12365478965133</td>
+					<td>mishanregmi@gmail.com</td>
+					<td>mishan.com</td>
+					<td>9841562546</td>
+				</tr>
+			</tbody>
+		</table>		
+	</div>
+</div>
