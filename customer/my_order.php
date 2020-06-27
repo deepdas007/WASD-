ff8a99908325894b741887c4b0501e7e6f786592
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-06-27 08:52:15 +0530

    Add files via upload

---
+++ customer/my_order.php
@@ -0,0 +1,59 @@
+<div>
+	<center>
+		<h1>
+			My Order
+		</h1>
+		<p>If there is any problem in your cart feel free to<a href="../contactus.php"> contact us</a> .</p>
+	</center>
+	<hr>
+	<div class="table-responsive">
+		<table class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>Serial No.</th>
+					<th>Due Amount</th>
+					<th>Invoice No.</th>
+					<th>Quality</th>
+					<th>Edition</th>
+					<th>Order Date</th>
+					<th>Paid/Unpaid</th>
+					<th>Status</th>
+				</tr>
+			</thead>	
+
+			<tbody>
+				<tr>
+					<td>#1</td>
+					<td>INR 200</td>
+					<td>554541</td>
+					<td>2</td>
+					<td>Gold Edition</td>
+					<td>2020-March-5</td>
+					<td>Unpaid</td>
+					<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+				</tr>
+				<tr>
+					<td>#2</td>
+					<td>INR 200</td>
+					<td>554541</td>
+					<td>2</td>
+					<td>Gold Edition</td>
+					<td>2020-March-5</td>
+					<td>Unpaid</td>
+					<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+				</tr>
+				<tr>
+					<td>#1</td>
+					<td>INR 200</td>
+					<td>554541</td>
+					<td>2</td>
+					<td>Gold Edition</td>
+					<td>2020-March-5</td>
+					<td>Unpaid</td>
+					<td><a href="confirm.php" target="_blank" class="btn btn-primary btn-sm">Confirm If Paid</a></td>
+				</tr>
+			</tbody>		
+		</table>
+	</div>
+
+</div>
