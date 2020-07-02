52660828e12a9e1c352e50702138acc30f9e8794
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-07-02 17:48:23 +0530

    Add files via upload

---
+++ customer/edit_act.php
@@ -0,0 +1,49 @@
+<div class="box">
+	<center>
+		<h1>Edit Your Account</h1>
+	</center>
+
+			<div class="form-group">
+				<label>Customer Name</label>
+				<input type="text" name="c_name" required="" class="form-control">				
+			</div>
+
+			<div class="form-group">
+				<label>Customer Email</label>
+				<input type="text" name="c_email" required="" class="form-control">				
+			</div>
+			
+			<div class="form-group">
+				<label>Customer Password</label>
+				<input type="password" name="c_pasword" required="" class="form-control">				
+			</div>
+			
+			<div class="form-group">
+				<label>Country</label>
+				<input type="text" name="c_country" required="" class="form-control">				
+			</div>
+			
+			<div class="form-group">
+				<label>City</label>
+				<input type="text" name="c_city" required="" class="form-control">				
+			</div>
+			
+			<div class="form-group">
+				<label>Contact Number</label>
+				<input type="text" name="c_number" required="" class="form-control">				
+			</div>
+			
+			<div class="form-group">
+				<label>Address</label>
+				<input type="text" name="c_address" required="" class="form-control">				
+			</div>
+			<div class="form-group">
+				<label>Image</label>
+				<input type="file" name="c_image" required="" class="form-control">	
+				<img src="customer_images/mishan.jpg" class="img-responsive" height="100" width="100">			
+			</div>
+
+			<div class="text=center">
+				<button class="btn btn-primary" name="update">Update Now</button>
+			</div>
+</div>
