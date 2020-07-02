d6aedee32a669383e7bf8ed654ec0687a1465993
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-07-02 17:49:16 +0530

    Add files via upload

---
+++ customer/change_password.php
@@ -0,0 +1,24 @@
+<div class="box">
+	<center>
+		<h3>Change Your Password</h3>
+	</center>
+	
+	<div class="form-group"><!--  //boostarp div to ake form responsive -->
+		<label>Enter Your Current Password</label>
+		<input type="password" name="old_password" class="form-control">		
+	</div>
+
+	<div class="form-group">
+		<label>Enter New Password</label>
+		<input type="password" name="new_password" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label>Confirm New Password</label>
+		<input type="password" name="c_n_password" class="form-control">
+	</div>
+
+	<div class="text-center">
+		<button class="btn btn-primary btn-lg">Update Now</button>
+	</div>
+</div>
