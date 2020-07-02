7f4b029d5be2f253344b3884adf8bc968dd0fcac
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-07-02 17:48:38 +0530

    Add files via upload

---
+++ customer/delete_ac.php
@@ -0,0 +1,11 @@
+<div class="box">
+	<center>
+		<h1>Do You Really Want To Delete Your Account?</h1>
+		<br>
+		<br>
+	<form action="" method="post">
+		<input type="submit" name="yes" value="Yes, I Want To Delete." class="btn btn-danger">
+		<input type="submit" name="no" value="No, I Changed My Mind." class="btn btn-primary">		
+	</form>
+	</center>
+</div>
