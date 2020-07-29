70ec12f113e48466c3212f1e8aa308f8fa75f174
Deep Das <61802644+deepdas007@users.noreply.github.com>
2020-07-29 23:02:34 +0530

    Add files via upload

---
+++ admin_area/includes/sidebar.php
@@ -0,0 +1,29 @@
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading"> <!-- //starting panel heading -->
+		<h3 class="panel-title">Product Platform</h3>
+	</div> <!-- //closing panel heading -->
+	<div class="panel-body">
+		<ul class= "nav nav-pills nav-stacked category-menu">		
+			<li><a href="shop.php">	PC </a></li>
+			<li><a href="shop.php">	XBox </a></li>
+			<li><a href="shop.php">	PS4 </a></li>
+
+		</ul>
+		
+	</div>
+</div>
+
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading"> <!-- //starting panel heading -->
+		<h3 class="panel-title">Product Category</h3>
+	</div> <!-- //closing panel heading -->
+	<div class="panel-body">
+		<ul class= "nav nav-pills nav-stacked category-menu">		
+			<li><a href="shop.php">	Fight </a></li>
+			<li><a href="shop.php">	Race </a></li>
+			<li><a href="shop.php">	Adventure </a></li>
+
+		</ul>
+		
+	</div>
+</div>
